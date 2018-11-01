db04422c81b7c8283d4946ddb9619442c4d2e6ad
Patricia Nykaza <pnykaza@calstatela.edu>
2018-11-01 20:10:11 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :new, :createl]
   get '/students/:id', to: 'students#show', as: 'student'
-  post '/students/new', to: 'students#new'
 end
